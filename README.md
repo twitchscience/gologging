5587a0a0a6db26e14a2cf37db9cd99c2eedf3b9d
Kai Hayashi <kai.hayashi@twitch.tv>
2014-07-16 17:09:53 -0700

    migrated repos

---
+++ README.md
@@ -0,0 +1,44 @@
+# Go Logging
+
+This project is the crux of our stats pipeline. It contains structures to write and
+rotate logs files in a fast non-blocking manner. The top levelt interface here is the
+Logger interface. The main implementer of this interface is the FileLogS3Writer.
+
+The FileLogS3Writer is a Logger that is thread safe. The writer will continue to write to a log
+until a rotate condition is met. The Writer is implemented with a writer stack. Once a file is
+rotated the FileLogS2Writer maintains an uploader that will upload the file to S3 with a 
+certain name structure.
+
+The Writer Stack maintains two io.WriterClosers. The first is the current WriterCloser.
+This is the writer that the stack activly writes to. The second if the next WriterCloser.
+This is the writer that the stack will switch to using immediatly after recieving a rotate.
+This architecture allows the WriterStack to continue to log messages in a (nearly)
+nonblocking manner even through rotations.
+
+## Example
+
+This project contains a simple example under src/examples
+
+# License
+The MIT License (MIT)
+
+Copyright (c) 2014 Twitch Interactive, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the Software), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
---
+++ examples/basic.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/TwitchScience/aws_utils/uploader"
+	"github.com/TwitchScience/gologging/gologging"
+	gen "github.com/TwitchScience/gologging/key_name_generator"
+	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/s3"
+)
+
+const targetBucket = "twlogger-test"
+
+var rps = flag.Int("rps", 1000, "requests / sec")
+
+type stdoutNotifier struct{}
+type stderrNotifier struct{}
+
+type localInstanceFetcher struct{}
+
+func (s *stdoutNotifier) SendMessage(r *uploader.UploadReceipt) error {
+	fmt.Println(r.KeyName)
+	return nil
+}
+func (s *stderrNotifier) SendError(e error) {
+	log.Println(e)
+}
+
+func (f *localInstanceFetcher) GetHost() string {
+	return "basic-host"
+}
+
+func (f *localInstanceFetcher) GetClusterName() string {
+	return "basic-test"
+}
+
+func (f *localInstanceFetcher) GetAutoScaleGroup() string {
+	return "basic-asg"
+}
+
+const (
+	MAX_LINES_PER_LOG = 10000 // 10 thousand requests.
+	// At 20k rps and 9 nodes we will rotate about every 10 mins.
+	JITTER = 20
+)
+
+var (
+	// Add jitter to the max lines. Draw a random number distributed normally with std deviation mlpl/2000.
+	adjustedMaxLines = int(MAX_LINES_PER_LOG + rand.New(rand.NewSource(time.Now().UnixNano())).NormFloat64()*float64(JITTER))
+)
+
+func main() {
+	flag.Parse()
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatalln("Failed to recieve auth from env")
+	}
+	awsConnection := s3.New(
+		auth,
+		aws.USWest2,
+	)
+	bucket := awsConnection.Bucket(targetBucket)
+	info := gen.BuildInstanceInfo(&localInstanceFetcher{}, "basic_example", ".")
+	rotateCoordinator := gologging.NewRotateCoordinator(adjustedMaxLines, time.Hour*1)
+	logger, err := gologging.StartS3Logger(
+		rotateCoordinator,
+		info,
+		&stdoutNotifier{},
+		&uploader.S3UploaderBuilder{
+			Bucket:           bucket,
+			KeyNameGenerator: &gen.EdgeKeyNameGenerator{info},
+		},
+		&stderrNotifier{},
+		5,
+	)
+	if err != nil {
+		log.Fatalf("Error building uploader: %s\n ", err)
+	}
+
+	i := 0
+	now := time.Now()
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		<-sigc
+		logger.Close()
+		fmt.Printf("Produced %f rps\n", float64(i)/(float64(time.Now().Sub(now))/float64(time.Second)))
+		os.Exit(0)
+	}()
+	x := int(time.Second) / *rps
+	for ; i < MAX_LINES_PER_LOG*4; i++ {
+		// throttle - there is a better solution to this
+		defer func() {
+			if x := recover(); x != nil { // means we cuagh a signal
+				time.Sleep(120 * time.Second)
+			}
+		}()
+		time.Sleep(time.Duration(int(0.8 * float64(x))))
+		logger.Log("MOAR! %d", i)
+	}
+	logger.Close()
+}
